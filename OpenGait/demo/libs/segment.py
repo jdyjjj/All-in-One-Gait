a56861c1e1a5783ce57182908792911c607ab74c
jdyjjj <1410234026@qq.com>
2023-02-08 22:04:27 +0800

    add track()

--- OpenGait/demo/libs/segment.py
+++ OpenGait/demo/libs/segment.py
@@ -1,4 +1,4 @@
-import os
+import os 
 import os.path as osp
 import sys
 import cv2
@@ -15,19 +15,17 @@ from tracker.byte_tracker import BYTETracker
 from tracking_utils.timer import Timer
 from tracking_utils.visualize import plot_tracking, plot_track
 from pretreatment import pretreat, img2pickle
-
-sys.path.append((os.path.dirname(os.path.abspath(__file__))) + "/paddle/")
+sys.path.append((os.path.dirname(os.path.abspath(__file__) )) + "/paddle/")
 from seg_demo import seg_image
 from yolox.exp import get_exp
 
-seg_cfgs = {
-    "model": {
-        "seg_model": "./demo/checkpoints/seg_model/human_pp_humansegv2_mobile_192x192_inference_model_with_softmax/deploy.yaml",
-        # 3
-        "ckpt": "./demo/checkpoints/bytetrack_model/bytetrack_x_mot17.pth.tar",  # 1
-        "exp_file": "./demo/checkpoints/bytetrack_model/yolox_x_mix_det.py",  # 4
+seg_cfgs = {  
+    "model":{
+        "seg_model" : "./demo/checkpoints/seg_model/human_pp_humansegv2_mobile_192x192_inference_model_with_softmax/deploy.yaml",# 3
+        "ckpt" :    "./demo/checkpoints/bytetrack_model/bytetrack_x_mot17.pth.tar",# 1
+        "exp_file": "./demo/checkpoints/bytetrack_model/yolox_x_mix_det.py", # 4
     },
-    "gait": {
+    "gait":{
         "dataset": "GREW",
     },
     "device": "gpu",
@@ -51,12 +49,10 @@ def loadckpt(exp):
     model = model.half()
     return model
 
-
 exp = get_exp(seg_cfgs["model"]["exp_file"], None)
 model = loadckpt(exp)
 
-
-def imageflow_demo(video_path, video_save_folder, sil_save_path):
+def track(video_path, video_save_folder):
     trt_file = None
     decoder = None
     device = torch.device("cuda" if seg_cfgs["device"] == "gpu" else "cpu")
@@ -80,11 +76,10 @@ def imageflow_demo(video_path, video_save_folder, sil_save_path):
 
     save_video_name = save_video_name.split(".")[0]
     results = []
+    track_results={}
     mark = True
     diff = 0
     while True:
-        # if frame_id == 90:
-        #     break
         print("frame {} begins".format(frame_id))
         if frame_id % 20 == 0:
             logger.info('Processing frame {} ({:.2f} fps)'.format(frame_id, 1. / max(1e-5, timer.average_time)))
@@ -109,39 +104,10 @@ def imageflow_demo(video_path, video_save_folder, sil_save_path):
                         online_tlwhs.append(tlwh)
                         online_ids.append(tid)
                         online_scores.append(t.score)
-                        print(
-                            f"{frame_id},{tid},{tlwh[0]:.2f},{tlwh[1]:.2f},{tlwh[2]:.2f},{tlwh[3]:.2f},{t.score:.2f},-1,-1,-1")
-
-                        tidstr = "{:03d}".format(tid)
-                        savesil_path = osp.join(sil_save_path, save_video_name, tidstr, "undefined")
-
-                        x = tlwh[0]
-                        y = tlwh[1]
-                        width = tlwh[2]
-                        height = tlwh[3]
-
-                        x1, y1, x2, y2 = int(x), int(y), int(x + width), int(y + height)
-                        w, h = x2 - x1, y2 - y1
-                        x1_new = max(0, int(x1 - 0.1 * w))
-                        x2_new = min(int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(x2 + 0.1 * w))
-                        y1_new = max(0, int(y1 - 0.1 * h))
-                        y2_new = min(int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)), int(y2 + 0.1 * h))
-
-                        new_w = x2_new - x1_new
-                        new_h = y2_new - y1_new
-                        tmp = frame[y1_new: y2_new, x1_new: x2_new, :]
-
-                        save_name = "{:03d}-{:03d}.png".format(tid, frame_id)
-                        side = max(new_w, new_h)
-                        tmp_new = [[[255, 255, 255]] * side] * side
-                        tmp_new = np.array(tmp_new)
-                        width = math.floor((side - new_w) / 2)
-                        height = math.floor((side - new_h) / 2)
-                        tmp_new[int(height):int(height + new_h), int(width):int(width + new_w), :] = tmp
-                        tmp_new = tmp_new.astype(np.uint8)
-                        tmp = cv2.resize(tmp_new, (192, 192))
-
-                        seg_image(tmp, seg_cfgs["model"]["seg_model"], save_name, savesil_path)
+                        print(f"{frame_id},{tid},{tlwh[0]:.2f},{tlwh[1]:.2f},{tlwh[2]:.2f},{tlwh[3]:.2f},{t.score:.2f},-1,-1,-1")
+                        if frame_id not in track_results:
+                            track_results[frame_id] = []
+                        track_results[frame_id].append([tid, tlwh[0], tlwh[1], tlwh[2], tlwh[3]])
                         results.append(
                             f"{frame_id},{tid},{tlwh[0]:.2f},{tlwh[1]:.2f},{tlwh[2]:.2f},{tlwh[3]:.2f},{t.score:.2f},-1,-1,-1\n"
                         )
@@ -166,8 +132,64 @@ def imageflow_demo(video_path, video_save_folder, sil_save_path):
         with open(res_file, 'w') as f:
             f.writelines(results)
         logger.info(f"save results to {res_file}")
-    return Path(sil_save_path, save_video_name)
+    return track_results
 
+def imageflow_demo(video_path, track_result, sil_save_path):
+    cap = cv2.VideoCapture(video_path)
+    width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)  # float
+    height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)  # float
+    frame_id = 0
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    save_video_name = video_path.split("/")[-1]
+
+    save_video_name = save_video_name.split(".")[0]
+    results = []
+    ids = list(track_result.keys())
+    while True:
+        ret_val, frame = cap.read()
+        if ret_val:
+            if frame_id in ids:
+                print("frame {} begins".format(frame_id))
+                for tidxywh in track_result[frame_id]:
+                    tid = tidxywh[0]
+                    tidstr = "{:03d}".format(tid)
+                    savesil_path = osp.join(sil_save_path, save_video_name, tidstr, "undefined")
+
+                    x = tidxywh[1]
+                    y = tidxywh[2]
+                    width = tidxywh[3]
+                    height = tidxywh[4]
+
+                    x1, y1, x2, y2 = int(x), int(y), int(x + width), int(y + height)
+                    w, h = x2 - x1, y2 - y1
+                    x1_new = max(0, int(x1 - 0.1 * w))
+                    x2_new = min(int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(x2 + 0.1 * w))
+                    y1_new = max(0, int(y1 - 0.1 * h))
+                    y2_new = min(int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)), int(y2 + 0.1 * h))
+                    
+                    new_w = x2_new - x1_new
+                    new_h = y2_new - y1_new
+                    tmp = frame[y1_new: y2_new, x1_new: x2_new, :]
+
+                    save_name = "{:03d}-{:03d}.png".format(tid, frame_id)
+                    side = max(new_w,new_h)
+                    tmp_new = [[[255,255,255]]*side]*side
+                    tmp_new = np.array(tmp_new)
+                    width = math.floor((side-new_w)/2)
+                    height = math.floor((side-new_h)/2)
+                    tmp_new[int(height):int(height+new_h),int(width):int(width+new_w),:] = tmp
+                    tmp_new = tmp_new.astype(np.uint8)
+                    tmp = cv2.resize(tmp_new,(192,192))
+
+                    seg_image(tmp, seg_cfgs["model"]["seg_model"], save_name, savesil_path)
+
+            ch = cv2.waitKey(1)
+            if ch == 27 or ch == ord("q") or ch == ord("Q"):
+                break
+        else:
+            break
+        frame_id += 1
+    return Path(sil_save_path, save_video_name)
 
 def writeresult(pgdict, video_path, video_save_folder):
     device = torch.device("cuda" if seg_cfgs["device"] == "gpu" else "cpu")
@@ -181,7 +203,7 @@ def writeresult(pgdict, video_path, video_save_folder):
     fps = cap.get(cv2.CAP_PROP_FPS)
     os.makedirs(video_save_folder, exist_ok=True)
     save_video_name = video_path.split("/")[-1]
-    save_video_path = save_video_name.split(".")[0] + "-After.mp4"
+    save_video_path = save_video_name.split(".")[0]+ "-After.mp4"
     save_video_path = osp.join(video_save_folder, save_video_path)
     print(f"video save_path is {save_video_path}")
     vid_writer = cv2.VideoWriter(
@@ -229,8 +251,7 @@ def writeresult(pgdict, video_path, video_save_folder):
                         )
                 timer.toc()
                 online_im = plot_track(
-                    img_info['raw_img'], online_tlwhs, online_ids, online_colors, frame_id=frame_id + 1,
-                    fps=1. / timer.average_time
+                    img_info['raw_img'], online_tlwhs, online_ids, online_colors, frame_id=frame_id + 1, fps=1. / timer.average_time
                 )
             else:
                 timer.toc()
@@ -251,15 +272,13 @@ def writeresult(pgdict, video_path, video_save_folder):
             f.writelines(results)
         logger.info(f"save results to {res_file}")
 
-
 def seg(video_path, video_save_folder, sil_save_path):
     sil_save_path = imageflow_demo(video_path, video_save_folder, sil_save_path)
     feats = img2pickle(Path(sil_save_path), 64, False, seg_cfgs["gait"]["dataset"])
     return feats
 
-
 def getsil(video_path, sil_save_path):
     sil_save_name = video_path.split("/")[-1]
-    feats = img2pickle(Path(sil_save_path, sil_save_name.split(".")[0]),
-                       64, False, seg_cfgs["gait"]["dataset"])
-    return (feats)
+    feats = img2pickle(Path(sil_save_path, sil_save_name.split(".")[0]), 
+                64, False, seg_cfgs["gait"]["dataset"])
+    return(feats)
